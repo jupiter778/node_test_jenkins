bf79225cb4db95f7cbba0061f15748f3356f6851
Your Name <saeaeibb@gmail.com>
2026-01-21 14:29:37 +0700

    deploy-docker3

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require('express')
 var app = express()
 
-app.set('port', (process.env.PORT || 5000))
+const PORT = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(PORT, '0.0.0.0', function() {
+  console.log(`Node app is running at 0.0.0.0:${PORT}`)
 })
